e12a6569532b566eac8f3243d32b15d6365f3d61
pacs <pacs@outlook.pt>
2022-04-03 17:00:36 +0100

    pagina inicial a renderizar

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+
+require('dotenv').config();
+
+const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+const PORT = process.env.PORT || 8000;
+
+
+app.get('/', (req,res) => {
+    res.render('index.hbs', {
+        title: 'Coisas a transportar'
+    });
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+
