3ed0076e807db0b32710ebc1f37067ac5756f698
pacs <pacs@outlook.pt>
2022-04-11 20:43:12 +0100

    algumas alterações no ficheiro principal de postgres

--- services/postgres.js
+++ services/postgres.js
@@ -1,9 +1,17 @@
 const { Client } = require('pg');
+require('dotenv').config();
 
-const client = new Client();
-
-await client.connect();
-
-const res = await client.query('SELECT $1::text as message', ['Hello world!'])
-console.log(res.rows[0].message) // Hello world!
-await client.end()
+(async () => {
+  const client = new Client({
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    ssl: true,
+  });
+  await client.connect();
+  const res = await client.query('SELECT $1::text as connected', ['Connection to postgres successful!']);
+  console.log(res.rows[0].connected);
+  await client.end();
+})();
