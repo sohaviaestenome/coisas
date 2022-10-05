dee53efd9e5520a486981836c32878568ee9eacf
pacs <pacs@outlook.pt>
2022-10-05 16:04:28 +0100

    clean up

--- backend/db/index.test.js
+++ backend/db/index.test.js
@@ -5,8 +5,7 @@ describe('testing postgres', () => {
 
     let pgPool;
     const isProduction = process.env.NODE_ENV === 'production';
-
-
+    
     const connectionString = `postgresql://${process.env.DB_USER}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
     beforeAll(() => {
