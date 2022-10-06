cdacdbfd123e6e47c1cc99216500abed50bdfbaa
pacs <pacs@outlook.pt>
2022-10-06 22:39:59 +0100

    still not working

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -3,9 +3,11 @@ import {getCoisas} from '../../apis/coisas';
 import React, { useEffect, useState } from 'react';
 import { CreateCoisa } from '../../components/PopUp/CreateCoisa/CreateCoisa';
 
+
 export const Home = () => {
   const [items, setItems] = useState([]);
   const [coisaLength, setCoisaLength] = useState([100]);
+  const [destinations, setDestinations] = useState([]);
 
   useEffect(() => {
     getCoisas().then(res =>{
@@ -13,12 +15,50 @@ export const Home = () => {
     })
   }, [coisaLength])
 
+
+
+useEffect(() => {
+  const arr1 = [];
+  items.forEach((item) => {
+    arr1.push([item.origem, item.destino]);
+  });
+  console.log(arr1, 'arr1');
+
+  arr1.forEach(pair => {
+    if (!destinations.length) {
+      setDestinations(destinations.push(pair));
+    }
+
+    destinations.forEach(destination => {
+      if(pair[0]===destination[0] && pair[1]=== destination[1]) {
+        return;
+        }else {
+          setDestinations(previous => [...previous, pair]);
+        }
+      }
+    )
+  });  
+   
+}, [items, destinations]);
+  console.log(destinations, 'destinations')
+
   return (
     <div>
       <h1>Coisas a Levar</h1>
       <CreateCoisa coisaLength = {coisaLength} setCoisaLength = {setCoisaLength}  />
       <div>
-        {items.map( item => <CoisasList {...item} key={item.id} coisaLength = {coisaLength} setCoisaLength = {setCoisaLength} items= {items} />)}
+        {items.map( item => {
+          return (
+            <CoisasList 
+            {...item} 
+            key={item.id} 
+            coisaLength = {coisaLength} 
+            setCoisaLength = {setCoisaLength} 
+            items= {items} 
+            destinations = {destinations}
+            />
+          )}
+        )}
       </div> 
     </div>
   )
