b236837455ae5f3105bc73ba32430c737c34ea3d
Pedro <pacs@outlook.pt>
2022-11-01 15:17:12 +0000

    connection string modificada para ter password

--- backend/db/index.js
+++ backend/db/index.js
@@ -4,7 +4,7 @@ const { DB } = require('../config')[NODE_ENV];
 const { Pool } = require('pg');
 const isProduction = NODE_ENV === 'production';
 
-const connectionString = `postgresql://${DB.DB_USER}@${DB.DB_HOST}:${DB.DB_PORT}/${DB.DB_DATABASE}`;
+const connectionString = DB.DB_PASSWORD ? `postgresql://${DB.DB_USER}:${DB.DB_PASSWORD}@${DB.DB_HOST}:${DB.DB_PORT}/${DB.DB_DATABASE}`: `postgresql://${DB.DB_USER}@${DB.DB_HOST}:${DB.DB_PORT}/${DB.DB_DATABASE}`;
 
 const db = new Pool({
   connectionString: isProduction ? DB.DATABASE_URL : connectionString,
